0b0b647cf6e9ebd09e91472c83228b794d05ca3c
Tiaan du Plessis <tiaanduplessis@hotmail.com>
2019-04-14 01:01:26 +0200

    Init

---
+++ README.md
@@ -0,0 +1,67 @@
+
+# next-styled-nprogress
+[![package version](https://img.shields.io/npm/v/next-styled-nprogress.svg?style=flat-square)](https://npmjs.org/package/next-styled-nprogress)
+[![package downloads](https://img.shields.io/npm/dm/next-styled-nprogress.svg?style=flat-square)](https://npmjs.org/package/next-styled-nprogress)
+[![standard-readme compliant](https://img.shields.io/badge/readme%20style-standard-brightgreen.svg?style=flat-square)](https://github.com/RichardLitt/standard-readme)
+[![package license](https://img.shields.io/npm/l/next-styled-nprogress.svg?style=flat-square)](https://npmjs.org/package/next-styled-nprogress)
+[![make a pull request](https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=flat-square)](http://makeapullrequest.com)
+
+> Add NProgress loader between pages in your Next.js App
+
+## Table of Contents
+
+- [Install](#install)
+- [Usage](#usage)
+- [Contribute](#contribute)
+- [License](#License)
+
+## Install
+
+This project uses [node](https://nodejs.org) and [npm](https://www.npmjs.com). It requires [Next](https://nextjs.org), [React](https://reactjs.org) and [styled-components](https://www.styled-components.com) as peer dependencies.
+
+```sh
+$ npm install next-styled-nprogress
+$ # OR
+$ yarn add next-styled-nprogress
+```
+
+## Usage
+
+In `_app.js`:
+
+```js
+import PageNProgress from 'next-styled-nprogress'
+
+class Example extends App {
+    // ...
+    render () {
+
+        return (
+        <Container>
+            <GlobalStyles />
+
+            <PageNProgress
+                color='#F2A83B'
+                showSpinner={false}
+                height='5px'
+                delay={200}
+            />
+
+            <Component />
+        </Container>
+        )
+    }
+}
+```
+
+## Contribute
+
+1. Fork it and create your feature branch: `git checkout -b my-new-feature`
+2. Commit your changes: `git commit -am "Add some feature"`
+3. Push to the branch: `git push origin my-new-feature`
+4. Submit a pull request
+
+## License
+
+MIT
+    
---
+++ src/index.js
@@ -0,0 +1,132 @@
+import React, { Component } from 'react'
+import NProgress from 'nprogress'
+import Router from 'next/router'
+import { createGlobalStyle } from 'styled-components'
+
+class PageNProgress extends Component {
+  constructor (props) {
+    super(props)
+
+    const { color, height } = this.props
+
+    // NOTE: Extracted from https://github.com/rstacruz/nprogress/blob/master/nprogress.css
+    this.NProgressStyles = createGlobalStyle`
+        /* Make clicks pass-through */
+        #nprogress {
+        pointer-events: none;
+        }
+
+        #nprogress .bar {
+        background: ${color};
+        position: fixed;
+        z-index: 1031;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: ${height};
+        }
+
+        /* Fancy blur effect */
+        #nprogress .peg {
+        display: block;
+        position: absolute;
+        right: 0px;
+        width: 100px;
+        height: 100%;
+        box-shadow: 0 0 10px ${color}, 0 0 5px ${color};
+        opacity: 1.0;
+        transform: rotate(3deg) translate(0px, -4px);
+        }
+
+        /* Remove these to get rid of the spinner */
+        #nprogress .spinner {
+        display: block;
+        position: fixed;
+        z-index: 1031;
+        top: 15px;
+        right: 15px;
+        }
+
+        #nprogress .spinner-icon {
+        width: 18px;
+        height: 18px;
+        box-sizing: border-box;
+        border: solid 2px transparent;
+        border-top-color: ${color};
+        border-left-color: ${color};
+        border-radius: 50%;
+        animation: nprogress-spinner 400ms linear infinite;
+        }
+
+        .nprogress-custom-parent {
+        overflow: hidden;
+        position: relative;
+        }
+
+        .nprogress-custom-parent #nprogress .spinner,
+        .nprogress-custom-parent #nprogress .bar {
+        position: absolute;
+        }
+
+        @-webkit-keyframes nprogress-spinner {
+        0%   { -webkit-transform: rotate(0deg); }
+        100% { -webkit-transform: rotate(360deg); }
+        }
+        @keyframes nprogress-spinner {
+        0%   { transform: rotate(0deg); }
+        100% { transform: rotate(360deg); }
+        }
+    `
+  }
+
+  static defaultProps = {
+    height: '2px',
+    color: '#29d',
+    delay: 0
+  }
+
+  componentDidMount () {
+    const {
+      delay,
+      easing,
+      speed,
+      minimum,
+      trickle,
+      trickleSpeed,
+      showSpinner
+    } = this.props
+
+    NProgress.configure({
+      easing,
+      speed,
+      minimum,
+      trickle,
+      trickleSpeed,
+      showSpinner
+    })
+
+    Router.events.on('routeChangeStart', () => {
+      this.timeout = setTimeout(() => NProgress.start(), delay)
+    })
+    Router.events.on('routeChangeComplete', () => {
+      clearTimeout(this.timeout)
+      NProgress.done()
+    })
+    Router.events.on('routeChangeError', () => {
+      clearTimeout(this.timeout)
+      NProgress.done()
+    })
+  }
+
+  shouldComponentUpdate = () => false
+
+  inc = val => NProgress.inc(val)
+
+  done = val => NProgress.done(val)
+
+  render () {
+    return <this.NProgressStyles />
+  }
+}
+
+export default PageNProgress
